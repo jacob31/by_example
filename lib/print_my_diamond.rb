9d72792a09587bd9acb0b76d8abda0ebc7763fcc
jacob31 <bensilbernagel@gmail.com>
2019-03-17 18:41:27 -0500

    chapter 2 exercises

--- lib/print_my_diamond.rb
+++ lib/print_my_diamond.rb
@@ -5,13 +5,14 @@ while rows.even?
   rows = gets.chomp.to_i
 end
 # print_diamond.rb
-
+# prints top half of diamond
 num_of_spaces = (rows/2) + 1
 (1..rows).step(2).each do |num_of_stars|
   num_of_spaces -= 1
   puts " " * num_of_spaces + "*" * num_of_stars
 end
 
+# prints bottom half of diamond
 (1..(rows-1)).step(2).reverse_each do |num_of_stars|
   num_of_spaces += 1
   puts " " * num_of_spaces + "*" * num_of_stars
---
+++ spec/print_my_diamond_spec.rb
@@ -0,0 +1,7 @@
+# spec/print_my_diamond_spec.rb
+require "print_my_diamond.rb
+
+describe PrintMyDiamond do
+
+end
+
