929a0c4078d00821632b3041f95d8a6dd79f794a
jacob31 <bensilbernagel@gmail.com>
2019-03-17 14:55:22 -0500

    put in loop to deal with input that is not an odd number

---
+++ print_my_diamond.rb
@@ -0,0 +1,18 @@
+# print_my_diamond.rb
+rows = 0
+while rows.even?
+  print "'Enter' the maximum number of rows (odd number):  "
+  rows = gets.chomp.to_i
+end
+# print_diamond.rb
+
+num_of_spaces = (rows/2) + 1
+(1..rows).step(2).each do |num_of_stars|
+  num_of_spaces -= 1
+  puts " " * num_of_spaces + "*" * num_of_stars
+end
+
+(1..(rows-1)).step(2).reverse_each do |num_of_stars|
+  num_of_spaces += 1
+  puts " " * num_of_spaces + "*" * num_of_stars
+end
