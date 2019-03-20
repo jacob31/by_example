0f52a554c34d14a673c728be4db3fdde50555d8b
jacob31 <bensilbernagel@gmail.com>
2019-03-19 19:27:45 -0500

    forgot to add in the number of guesses

--- lib/Chapter 3/number_guesses.rb
+++ lib/Chapter 3/number_guesses.rb
@@ -6,17 +6,17 @@ def request_guess
   return guess
 end
 
-count = 0
+count_guesses = 0
 answer = rand(9)
 guess = nil
 
 until guess == answer
 
   guess = request_guess
-  count += 1
+  count_guesses += 1
   case guess <=> answer
   when 1 then puts "Your guess was high."
-  when 0 then puts "Good guess!"
+  when 0 then puts "Good guess! You guessed #{count_guesses} times."
   when -1 then puts "Your guess was low."
   end
 end
