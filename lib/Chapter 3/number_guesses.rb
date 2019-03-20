7809773fea30230571d9de8aa3d39b62b2a6ee40
jacob31 <bensilbernagel@gmail.com>
2019-03-19 19:35:52 -0500

    changed verbage for guessing game

--- lib/Chapter 3/number_guesses.rb
+++ lib/Chapter 3/number_guesses.rb
@@ -6,17 +6,28 @@ def request_guess
   return guess
 end
 
+def other_guesses
+  puts "Let's try again..."
+  guess = gets.chomp.to_i
+  return guess
+end
+
 count_guesses = 0
 answer = rand(9)
 guess = nil
 
 until guess == answer
 
-  guess = request_guess
+  if count_guesses == 0
+    guess = request_guess
+  else
+    guess = other_guesses
+  end
+
   count_guesses += 1
   case guess <=> answer
   when 1 then puts "Your guess was high."
-  when 0 then puts "Good guess! You guessed #{count_guesses} times."
+  when 0 then puts "Good guess! It took #{count_guesses} guess#{ if guess == 1 then '' else 'es' end}."
   when -1 then puts "Your guess was low."
   end
 end
