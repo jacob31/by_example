8db84ab946fd2a8327719359242621a430dedffd
jacob31 <bensilbernagel@gmail.com>
2019-04-04 03:37:45 -0500

    refactored hangman

--- lib/chapter_3/hangman.rb
+++ lib/chapter_3/hangman.rb
@@ -1,37 +1,51 @@
 # hangman.rb
-require 'time'
-word_list = ['dictionary', 'university', 'employment', 'encourage', 'mountain', \
-             'tricycle', 'testimony', 'transportation', 'education', 'transformation']
 
-# guesses = word.chars.map { |element| element = nil}
 
-# puts "My secret word: '#{hide_word.join}'"
-# puts "Your guesses:    #{guesses}"
+  #start game
+  class Hangman
+    attr_accessor :format_word, :guess, :guessed_format, :char_guessed
 
-class Hangman
-
-  def initialize
-    puts "... populaitong word to be guessed."
-    sleep(0.5)
-    @@word = self.get_word
-  end
-
-  def request_input
-    puts "Key in a character or quess the word."
-    input = gets.chomp
-  end
-
-  def self.get_word
-    word = "dictionary"
-    return word
-  end
-  def self.hide_word
+    def initialize
+      @word = "health"
+      @len = @word.length
+      @guessed_format = []
+      @char_guessed = Array.new(@len, false)
+      self.format_word
+    end
     
+    def format_char(index)
+        form = nil
+        unless index == @len - 1
+          form = "_ "
+        else
+          form = "_"
+        end
+        return form
+    end
+
+  def request_guess
+    puts "Enter a character or enter your guess for the word."
+    @guess = gets.chomp
+  end
+
+  def format_word
+    guessed_format.clear
+    index = 0
+    self.char_guessed.each do |guessed|
+        if guessed
+          @guessed_format.append(@word[index])
+        else
+          @guessed_format.append(format_char(index))
+        end
+        index += 1
+    end
+    puts guessed_format.join
   end
 end
 
-
-
+  #allow the whole word to be guess also.
+  #   ex. "help" as "h e l p".
+  #Once the word has been guessed. Ask if they want to play again.
 
 
 
